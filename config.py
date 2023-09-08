f2fc21970577ceb56abdbe726ff502c77438a763
a2g-leo <142281343+a2g-leo@users.noreply.github.com>
2023-09-08 20:00:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ class Config:
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
     OWNER_ID = os.environ.get("OWNER_ID", "6515901556")                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://fast:leofast@cluster0.u8srkac.mongodb.net/?retryWrites=true&w=majority")  
-    DATABASE_NAME = os.environ.get("DATABASE_NAME", "tgcf-config")
-    COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward')
-    SESSION = os.environ.get("SESSION", "1BVtsOLEBuzeucprO-byVzWpPDQbIIxc3YjVLdajR80qJJIjeHWuIGH_4lw88uWaQBjPXGl-Hf-oBJUdmvQLBsEz7Mz_kvOFX2UJY4SI3hwfEdJ6HMy-JaaFoFkmCEvNHBqDgOCWNDu1eZ3PvwVAsPj2-c8F4mhnlRI-rGCOWYDjlc9NNsZTOG3oqye664RK2pneAUQublxtGjwW0SSJeLvAg3slJqYrTEdBUmm3VgKtbi1EXpkquiKHjpB2geIFHnLtdZoOSAFW7MW816OmlMaUJaqkXXqy331INy8eDDmQDwno7-SXbdpUAqFwm-aQIC7YySnYRDpMsaON0PNNafBAhLTuCrHc=")   
+    DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+    COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'fast')
+    SESSION = os.environ.get("SESSION", "1BVtsOJIBu8LWKvj7kPH5LlCEFvlBJKKYMYFanU5-O10w8B3IcuRhydRZpyaDX4n3iqK_Dq2DpTkHj2Uw6M-qVjckHvOWHigQaKzp9QyrpJ8gBnob2EjQNPLT45g9WZRIdVeadB1tdLtapdjxLG2__PI2ZNG8tUHDo8vYgLn10A751bV236TLboOQJJfgnenOnRMN8pGxA0PZbOtXer6FxppPMI0PgHrlJEAygfvikq-lYkKSdtrt5Pb8BRTUkqCmNBMW1pUQuU8PktAvhqiYZcy4dWPXaaR6iExv-BcRV6DoOjj7GhiPpqPd01EEYTULvBDMJVjzf9dttLmRJ5V79mVEHhx488M=")   
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001918596333"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", "uniquifyleo_bot")
 
